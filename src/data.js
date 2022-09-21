e354e12f3daa1f8eb2e64627e171dbdfd110dd4f
Geetham15 <Geetham15@gmail.com>
2022-09-21 11:25:13 -0600

    first commit for data set user reward management

---
+++ src/data.js
@@ -0,0 +1,60 @@
+module.exports = {
+    geetha: [
+        {
+        date : "1/1/2022",
+        amount: 100.00
+        },
+        {
+            date : "2/1/2022",
+            amount: 50
+        },
+        {
+            date : "3/1/2022",
+            amount: 150
+        },
+        {
+            date : "3/2/2022",
+            amount: 200
+        },
+        {
+            date : "4/1/2022",
+            amount: 200
+        }
+    ],
+    vignesh:[
+        {
+            date : "1/1/2022",
+            amount: 150.00
+            },
+            {
+                date : "2/1/2022",
+                amount: 100
+            },
+            {
+                date : "3/1/2022",
+                amount: 200
+            },
+            {
+                date : "4/1/2022",
+                amount: 250
+            }
+    ],
+    bob:[
+        {
+            date : "1/1/2022",
+            amount: 200.00
+            },
+            {
+                date : "2/1/2022",
+                amount: 150
+            },
+            {
+                date : "3/1/2022",
+                amount: 250
+            },
+            {
+                date : "4/1/2022",
+                amount: 300
+            }
+    ],
+}
